8ae10ea5c8654ee9074a59a3cbdc15c00a9bff8f
OrtizAlex <46968556+OrtizAlex@users.noreply.github.com>
2021-05-13 19:25:00 -0400

    edited html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,86 @@
+var originOptions = ["JFK", "LGA", "NWK", "MAD", "RIO"]; 
+var destinationOptions = ["JFK", "LGA", "NWK", "MAD", "RIO"]; 
+
+var originSelect = document.getElementById("origin"); 
+var destinationSelect = document.getElementById("destination"); 
+
+applyChoices(originOptions, originSelect);
+applyChoices(destinationOptions, destinationSelect);
+
+var searchBtn = document.getElementById("search");
+searchBtn.addEventListener("click", searchResults);
+
+var origin;
+var destination;
+var departureDate;
+
+function searchResults(){
+    origin = document.getElementById("origin").value;
+    destination = document.getElementById("destination").value;
+    departureDate = moment(document.getElementById("departure-date").value).format("YYYY-MM-DD");
+
+
+    flightSearch();
+    COVIDSearch();
+    
+
+}
+
+function flightSearch(){
+    fetch("https://test.api.amadeus.com/v1/security/oauth2/token",
+    {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: "grant_type=client_credentials&client_id=lnAAmWl7igs0Zz53pQS7zKu7Dh4cyqGA&client_secret=bsn0KuCd9bBXIVR6"
+    })
+    .then(processJSON)
+    .then(fetchFlight)
+    .then(processJSON)
+    .then(console.log)
+    .catch(err => {
+	    console.error(err);
+    });
+}
+
+function processJSON(response){
+	return response.json();
+};
+
+function fetchFlight(token){
+    console.log(token.access_token);
+    return fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&adults=1`,
+    {
+        headers: {"Authorization": "Bearer " + token.access_token }
+    });
+}
+
+function COVIDSearch(){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+    var COVIDorigin;
+    if(origin == "MAD")
+      COVIDorigin = "Spain";
+      
+    fetch(`https://api.covid19tracking.narrativa.com/api/country/${COVIDorigin}?date_from=${moment().format("YYYY-MM-DD")}`, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+}
+
+function applyChoices(options, select){
+    for(var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }
+}
+
 /*fetch("https://test.api.amadeus.com/v1/security/oauth2/token",
 {
     method: "POST",
@@ -22,7 +105,7 @@ function fetchFlight(token){
     {
         headers: {"Authorization": "Bearer " + token.access_token }
     });
-}*/
+}
 
 var requestOptions = {
     method: 'GET',
@@ -33,5 +116,7 @@ fetch("https://api.covid19tracking.narrativa.com/api/country/spain?date_from=202
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
+
+*/
 
 
